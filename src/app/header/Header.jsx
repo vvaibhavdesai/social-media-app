240795b2f274f439e7e3457fbff88bce07ab482e
Vaibhav Desai <vaibhavdesai888@gmail.com>
2021-07-19 13:55:26 +0530

    created debounce for search

--- src/app/header/Header.jsx
+++ src/app/header/Header.jsx
@@ -9,52 +9,76 @@ import { CgProfile } from "react-icons/cg";
 import "../navbar/Navbar.css";
 import "./Header.css";
 import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
-import { axios } from 'axios';
+import { useState, useEffect, useCallback } from "react";
+import axios from "axios";
 
 export function Header() {
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState([]);
 
-  async function fetchSearch(query){
-    try{
-      const { data } = await axios.get(`https://socialMedia.vaibhavdesai888.repl.co/profile/search`,{
-        searchId:query
-      })
-      console.log(data,"yeh aya search ka data")
-    }catch(error){
-      console.log(error.message)
+  async function fetchSearch(event) {
+    const { value } = event.target;
+    console.log(value, "yeh hai search field");
+    try {
+      if(value){
+      const { data } = await axios.post(
+        `https://socialMedia.vaibhavdesai888.repl.co/profile/search`,
+        {
+          searchId: value,
+        }
+      );
+      console.log(data, "yeh aya search ka data");
+      setSearch(data.user)}
+      else{
+        setSearch([])
+      }
+    } catch (error) {
+      console.log(error.message);
     }
   }
+
+  const debouncer = (func, delay) => {
+    let timer;
+    return function (...args) {
+      const context = this;
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        func.apply(context, args);
+      }, delay);
+    };
+  };
+  const debounceCaller = useCallback(debouncer(fetchSearch, 500),[]);
 
   return (
     <header className="m-header">
       <h1 className="m-header-title">Antigram</h1>
       <span>
-        <input
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-          className="m-header-search"
-        />
-        {search ? (
+        <input onChange={debounceCaller} className="m-header-search" />
+        {search.length > 0 ? (
           <ul
             style={{
-              border:"5px solid lightgray",
-              borderRadius:"20px",
+              border: "5px solid lightgray",
+              borderRadius: "20px",
               position: "fixed",
               width: "20rem",
-              zIndex:"7",
+              zIndex: "7",
               textAlign: "left",
               listStyle: "none",
               padding: "0.2rem 0",
-              height:"7rem",
-              overflow:"scroll",
-              overflowX:"hidden"
+              height: "7rem",
+              overflow: "scroll",
+              overflowX: "hidden",
             }}
           >
-            <li style={{ margin: "0.7rem 0 0.5rem 0.5rem",borderBottom:"1px solid lightgray" }}>"helolo"</li>
-            <li style={{ margin: "0.5rem 0 0.5rem 0.5rem",borderBottom:"1px solid lightgray" }}>"helolo"</li>
-            <li style={{ margin: "0.5rem 0 0.5rem 0.5rem",borderBottom:"1px solid lightgray" }}>"helolo"</li>
-            <li style={{ margin: "0.5rem 0 0.5rem 0.5rem",borderBottom:"1px solid lightgray" }}>"helolo"</li>
+            {search.map(user=>
+            <li
+            key={user._id}
+              style={{
+                margin: "0.7rem 0 0.5rem 0.5rem",
+                borderBottom: "1px solid lightgray",
+              }}
+            >
+              {user.name}
+            </li>)}
           </ul>
         ) : (
           ""
